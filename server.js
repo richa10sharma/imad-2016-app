68c6fe9efa97b573ff4de7eab0b864d035ed5e5d
richa10sharma <sharmaricha1001@yahoo.com>
2016-09-26 10:45:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,46 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var articleOne ={
+    title: `Article-One`,
+    heading: `Article One`
+    date: `Sept 5`
+    content: `<p>
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                </p>
+                <p>
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                </p>
+                <p>
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                </p>
+                <p>
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                This is the first content for my first article
+                </p>`
+    
+    
+    
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
