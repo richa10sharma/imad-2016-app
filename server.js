4b343d9a8fc1bf32bd3449fad4fe544a4ec8956e
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-07 01:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,9 +58,8 @@ app.get('/ui/madi.png', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db',function(req, res){
-    //make a select request
-    //return a response
-    pool.query('SELECT * FROM test', function(err,result){
+
+    pool.query('SELECT * FROM test', function(err, result){
         if(err){
         res.status(500).send(err.toString());
             
