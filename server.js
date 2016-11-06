934e50f1c2a4e4f04d61fa0a1f4feaf25dc86e87
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-06 22:27:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     databse:'richa10sharma',
     host:'http://db.imad.hasura-app.io/',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 
