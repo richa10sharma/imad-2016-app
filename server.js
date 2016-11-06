130d1862a66cea958c829cd3b8fee9369bf792df
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-07 01:20:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/test-db',function(req, res){
 });
 
 //-------------------------------HASHING_____________________________
-function hash (input){
+function hash (input, salt){
     //how to create a hash 
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
