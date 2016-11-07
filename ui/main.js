bd7c2fcffd71ef9de1d62d7bb7dce09fef01847c
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-07 14:13:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
 console.log('Loaded!');
+var button = document.getElementById('main.txt');
+var counter = 0;
+button.onclick = function() {
+    
+    
+    var request = new XMLHttpRequest();
+    
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readystate === XMLHttpRequest.DONE){
+            
+            if(request.status === 200)
+            {
+                
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    
+};
