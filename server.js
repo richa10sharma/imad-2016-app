a5da4774265739dce5c60a11e0939c58e48684c7
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-07 10:23:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var bodyParser = require('body-parser');
 //var pool = new Pool(config);
 var crypto = require('crypto');
 var config = {
@@ -17,6 +17,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -46,6 +47,27 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var pool = new Pool(config);
+//---------------CREATE USER----------------------
+app.post('create-user', function (req, res) {
+  
+  var username = req.body.username;
+  var password = req.body.password;
+  var salt = crypto.getRandomBytes(128).toString('hex');
+  var dbString = hash(Password,salt);
+  
+  pool.query("INSERT INTO 'user' (username, password) VALUES ($1, $2)", [username, dbString], function(err, result){
+        if(err){
+        res.status(500).send(err.toString());
+            
+        }
+        else{
+            res.send('user successfully created: '+ username);
+        }
+    });
+  
+});
+
+
 app.get('/test-db',function(req, res){
 
     pool.query('SELECT * FROM test', function(err, result){
@@ -71,7 +93,7 @@ function hash (input, salt){
 }
 
 	
-	app.get('/hash/:input',function(req, res)
+	app.get('/hash/:input',function(req, res)// we took it to be /hash/:input we took ':' because we wanted to make the input as a part of URL 
         {
            var hashedString = hash(req.params.input, 'this-is-some-random-string');
            res.send(hashedString);
