a8c117d1b1eee154aa9833541f15c47c16d59774
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-18 07:46:35 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,8 +98,7 @@ function createTemplate (data) {
             <!--Import jQuery before materialize.js-->
             <script type="text/javascript" src="ui/materialize.min.js">
             </script>
-      <script type="text/javascript" src="/ui/main.js">
-        </script>
+    
         
        <script type="text/javascript" src="/ui/article.js">
         </script>
