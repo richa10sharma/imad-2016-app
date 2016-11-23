61f8c7250ff5f7510b4cf347b9b28d6c4519f57a
richa10sharma <sharmaricha1001@yahoo.com>
2016-11-23 18:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.post('/create-user', function (req, res) {
       }
    });
 });
-
+//--
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
